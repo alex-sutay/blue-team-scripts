c27c8f61dfb5c2a38b35fed81d943a099ab7401f
N42950M <tardistestpilot@gmail.com>
2023-10-03 22:14:18 -0400

    fixed mysql backup script, accidentally uploaded untested version

--- bash/backup_mysql.sh
+++ bash/backup_mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#backup mysql databases, put them in ~/database
+#backup mysql databases in the current directory
 
 if [ "$EUID" -ne 0 ]; then
     #if running as a normal user then modify this with the mysql credentials
@@ -11,11 +11,8 @@ else
     DB_PASSWORD=""
 fi
 
-#backup directory
-BACKUP_DIR="~/backups"
-
 #timestamp for backup filename
-TIMESTAMP=$(date +%Y%m%d%H%M%S)
+TIMESTAMP=$(date +%d%H%M%S)
 
 #get all databases
 DATABASES=$(mysql -u"$DB_USER" -p"$DB_PASSWORD" -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql)")
@@ -23,14 +20,14 @@ DATABASES=$(mysql -u"$DB_USER" -p"$DB_PASSWORD" -e "SHOW DATABASES;" | grep -Ev 
 #loop through each database and create a backup, backs them up in the current working directory
 #when running as root it prompts for password for every database, so just press enter when it pops up lol
 for DB_NAME in $DATABASES; do
-   mysqldump -u"$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql"
+   mysqldump -u"$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$DB_NAME-$TIMESTAMP.sql"
     
     # Check if the backup was successful
     if [ $? -eq 0 ]; then
-        echo "Backup completed successfully for $DB_NAME: $BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql"
+        echo "Backup completed successfully for $DB_NAME: $DB_NAME-$TIMESTAMP.sql"
     else
         echo "Backup failed for $DB_NAME"
     fi
 done
 
-#TO RESTORE ALL YOU NEED TO DO IS DO (as root) IS "mysql [database-name] < [backup].sql", making a script for this now
+#TO RESTORE ALL YOU NEED TO DO IS DO (as root) IS "mysql [database-name] < [backup].sql"
