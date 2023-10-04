1f4a4c36057384952a8bdfbb0710bf01e6a88d44
N42950M <tardistestpilot@gmail.com>
2023-10-03 20:53:28 -0400

    uninstalling python3 is a no go, it kills ufw (check installs.sh)

--- bash/installs.sh
+++ bash/installs.sh
@@ -9,5 +9,9 @@ apt-get -y install ${additions[@]}
 systemctl enable auditd
 
 #uninstalls, add any uninstalls to the variable
-removals=("python2" "python3" "python3-pip")
-apt-get remove ${removals[@]}
+#removals=("python2" "python3" "python3-pip")
+#apt-get remove ${removals[@]}
+
+#NOTE(from nate): So I did some testing on openstack and uninstalling python3 uninstalls ufw (and xorg so if we rebooted there'd be no x11 server)
+#so we cant really do that :/
+#proof of testing: https://pomf2.lain.la/f/z4dtre8.png & https://pomf2.lain.la/f/2g7awij6.png
